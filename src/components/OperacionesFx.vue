552d9a9ebf9b5b078ebb991647aa1f79d801d53b
Aldo <aldo.altamira1@gmail.com>
2022-04-13 11:06:19 -0500

    agregando transactionId con campo QuoteRedID

--- src/components/OperacionesFx.vue
+++ src/components/OperacionesFx.vue
@@ -635,7 +635,7 @@ export default {
         Side: this.opSide,
         Symbol: currenciesSelected,
         Product: 'SPOT',
-        TransactionId: `Request_Insert_01_${Math.floor(Math.random() * 1000)}`,
+        TransactionId: this.qQuoteReqID,
         RequestSystem: 'PORTALFX',
       };
       clearInterval(this.timmerId);
